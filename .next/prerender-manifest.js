87e7ed465c25fc8bbef3951ff0a356e4a48fcddc
Shrut Vanpariya <98260677+shrut-vanpariya@users.noreply.github.com>
2023-08-30 02:42:00 +0530

    changes

---
+++ .next/prerender-manifest.js
@@ -0,0 +1,1 @@
+self.__PRERENDER_MANIFEST="{\"version\":4,\"routes\":{},\"dynamicRoutes\":{},\"preview\":{\"previewModeId\":\"04bcee79cde635be3d337bd4275c8fde\",\"previewModeSigningKey\":\"8309cf0901177eccd6c9b92ff186bf1839f53b11e41f46c1853a97f699c565cd\",\"previewModeEncryptionKey\":\"898df6081cd49652dade1706c3ab6e865e28ffaa6b5dcc7572dda29943a67c40\"},\"notFoundRoutes\":[]}"
