18e97473e9e3d85c64d524225e69d5c210377558
Shrut Vanpariya <98260677+shrut-vanpariya@users.noreply.github.com>
2023-08-30 08:49:15 +0530

    changes

---
+++ .next/static/l93_eT69VlAHNXRv4v7nh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/200-a9f64c4992dd13ee.js","static/css/278e4a8016f73789.css","static/chunks/pages/index-c657a8bf27ac0a3a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
